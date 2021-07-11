58568c03b17d47c067a5601faedfe8416f673fc4
excircle <unixdisciple.us@gmail.com>
2021-07-10 18:38:17 -0700

    Functional nginx eks cluster

--- starter-eks/variables.tf
+++ starter-eks/variables.tf
@@ -1,35 +1,57 @@
 variable "region" {
-    default     = "us-east-1"
-    description = "AWS Region"
+  default     = "us-east-1"
+  description = "AWS region"
 }
 
 variable "cluster_name" {
-    default = "starter-eks"
+  default = "getting-started-eks"
 }
 
 variable "map_accounts" {
-    description = "Additional AWS account numbers to add to aws-auth configmap"
-    type        = list(string)
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
 
-    default     = [
-        "777777777777",
-        "888888888888",
-    ]
+  default = [
+    "777777777777",
+    "888888888888",
+  ]
 }
 
 variable "map_roles" {
-    description = "Additional IAM roles to add to the aws-auth configmap"
-    type        = list(object({
-        rolearn     = string
-        username    = string
-        groups      = list(string)
-    }))
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
 
-    default = [
-        {
-            userarn     = "arn:aws:iam::666666666666:user/user1"
-            username    = "user1"
-            groups      = ["system:masters"]
-        },
-    ]
+  default = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
 }
